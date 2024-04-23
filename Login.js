68f69cc698f149ba77b26fe4bd712c2e331710cb
ajaykumar <ajaykumar.18158@marwadieducation.edu.in>
2024-04-23 18:21:05 +0530

    first commit

---
+++ Login.js
@@ -0,0 +1,16 @@
+// document.getElementById("loginForm").addEventListener("submit", function (event) {
+//     event.preventDefault();
+
+//     var username = document.getElementById("Email").value;
+//     var password = document.getElementById("password").value;
+
+//     var validUsername = "ajay@123";
+//     var validPassword = "1234";
+
+//     if (username === validUsername && password === validPassword) {
+
+//         window.location.href = "./TodoPage.Html";
+//     } else {
+//         document.getElementById("error").innerText = "Invalid username or password!";
+//     }
+// });
